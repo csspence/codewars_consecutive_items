f840d5c92abdb878dae82dc31ec1f2a2e656987b
Crew Spence <csspence123@gmail.com>
2020-11-02 05:20:02 -0600

    Wrote working solution.

--- codewars_consecutive_items.js
+++ codewars_consecutive_items.js
@@ -4,6 +4,14 @@ You are given a list of unique integers arr, and two integers a and b. Your task
 It is guaranteed that a and b are both present in arr.
 */
 
-unction consecutive(arr, a, b) {
-  // Your code here...
+const consecutive = (arr, a, b) => {
+  for(let i = 0; i < arr.length - 1; i++) {
+    if(arr[i] === a && arr[i+1] === b) {
+      return true
+    }
+    if(arr[i] === b && arr[i+1] === a) {
+      return true
+    }
+  }
+  return false;
 }
